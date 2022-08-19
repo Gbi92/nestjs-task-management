48a88eebb05175e6dfd4071dcd5e2c4200a355d7
Gabriella Koos <gabriella.koos@traxretail.com>
2022-08-19 10:42:39 +0200

    data persistence - refactor task repository

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,8 +1,47 @@
-import { EntityRepository, Repository } from "typeorm";
+import { Injectable } from "@nestjs/common";
+import { DataSource } from "typeorm";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { TaskStatus } from "./task-status.enum";
 import { Task } from "./task.entity";
 
-@EntityRepository(Task)
-export class TasksRepository extends Repository<Task> {
-  // ToDo
-  // https://gist.github.com/anchan828/9e569f076e7bc18daf21c652f7c3d012
+@Injectable()
+export class Repositories {
+  constructor(private dataSource: DataSource) {}
+  get tasksRepository() {
+    return this.dataSource.getRepository(Task).extend({
+      async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
+
+        const query = this.createQueryBuilder('task');
+
+        if (status) {
+          query.andWhere('task.status = :status', { status });
+        }
+
+        if (search) {
+          query.andWhere(
+            'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+            { search: `%${search}%`},
+          );
+        }
+
+        const tasks = await query.getMany();
+        return tasks;
+      },
+
+      async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        const { title, description } = createTaskDto;
+
+        const task = this.create({
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        });
+
+        await this.save(task);
+        return task;
+      }
+    });
+  }
 }
