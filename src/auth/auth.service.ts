449946f904333b5f830edcd48a6ed83fd530164f
Gabriella Koos <gabriella.koos@traxretail.com>
2022-08-22 16:52:15 +0200

    sign up and sign in features

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthCredentialsDto } from 'src/tasks/dto/auth-credentials.dto';
 import { Repositories } from 'src/tasks/repositories';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +9,16 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.repositories.usersRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.repositories.usersRepository.findOne({where: { username: username }});
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return 'success';
+        } else {
+            throw new UnauthorizedException('Please check your login credentials');
+        }
     }
 }
