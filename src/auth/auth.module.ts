51984d2e15b69c722d0efb4a18b18443a4211ecf
Gabriella Koos <gabriella.koos@traxretail.com>
2022-08-31 15:49:51 +0200

    auth - jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repositories } from 'src/tasks/repositories';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
     TypeOrmModule.forFeature([User]),
   ],
   providers: [AuthService, Repositories],
